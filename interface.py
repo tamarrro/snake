9e0687b866e781c02cadef293e61b2d93c701a6f
tamarrro <114574485+tamarrro@users.noreply.github.com>
2022-11-28 21:31:26 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -4,15 +4,20 @@ from config import *
 
 def get_display():
    dis = pygame.display.set_mode((dis_width, dis_height))
+   ''' задаем размер игрового поля'''
    pygame.display.set_caption('Змейка от алычы')
+   ''' добавляем название игры'''
    return dis
  
-def Your_score(dis, score):
-   value = score_font.render("Ваш счёт: " + str(score), True, yellow)
-   dis.blit(value, [0, 0])
+def Your_score(dis, score): 
+   ''' создаем функцию, которая выводит на экран количество очков'''
+   value = score_font.render("Ваш счёт: " + str(score), True, pink)
+   dis.blit(value, [0, 550])
  
 
  
-def message(dis, msg, color):
+def message(dis, msg, color): 
+   '''создаем функцию, которая выводит сообщение на экран'''
    mesg = font_style.render(msg, True, color)
-   dis.blit(mesg, [dis_width / 6, dis_height / 3])
+   dis.blit(mesg, [dis_width / 12, dis_height / 2])
+
