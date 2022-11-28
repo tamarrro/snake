834ad8b4e217d3a82a2deb14296a527ad0e3f19a
tamarrro <114574485+tamarrro@users.noreply.github.com>
2022-11-28 21:30:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,21 @@
 import pygame
 
-white = (255, 255, 255)
+
 yellow = (255, 255, 102)
 black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
+red = (66, 49, 137)
+green = (23, 114, 69)
+blue = (102, 0, 102)
+purple = (204, 204, 255)
+pink = (102, 0,255)
+'''задаем цвета'''
 
-font_style = pygame.font.SysFont("bahnschrift", 25)
-score_font = pygame.font.SysFont("comicsansms", 35)
+font_style = pygame.font.SysFont("arial", 25)
+score_font = pygame.font.SysFont("arial", 35)
+'''выбираем шрифт для нашего текста'''
 
 dis_width = 800
 dis_height = 600
+'''задаем параметры экрана'''
 
 
